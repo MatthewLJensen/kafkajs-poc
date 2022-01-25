dc634d85b252c3d430fa42cc3839705646c810ae
Matthew Jensen <matthewljensen@gmail.com>
2022-01-25 16:55:38 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const produce = require("./producer")
+const stressTest = require("./stressTest")
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -8,37 +9,47 @@ const port = 3000
 let i = 0
 
 
-// We are using our packages here
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
- extended: true})); 
-app.use(cors())
+var args = process.argv.slice(2);
+if (args[0] == "stress") {
+    stressTest().catch((err) => {
+        console.error("error in stressTest: ", err)
+    })
+} else {
 
-//You can use this to check if your server is working
-app.get('/', (req, res)=>{
-res.send("Welcome to your server")
-})
+    // We are using our packages here
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
 
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }));
+    app.use(cors())
 
-//Route that handles login logic
-app.post('/produce', (req, res) =>{
-    produceEvent(i.toString(), req.body.lat, req.body.lng)
-    i++
-    res.send("Message sent")
-})
-
-
-//Start your server on a specified port
-app.listen(port, ()=>{
-    console.log(`Server is runing on port ${port}`)
-})
-
-
-function produceEvent(id, lat, lng) {
-    // call the `produce` function and log an error if it occurs
-    produce(id, lat, lng).catch((err) => {
-        console.error("error in producer: ", err)
+    //You can use this to check if your server is working
+    app.get('/', (req, res) => {
+        res.send("Welcome to your server")
     })
 
+
+    //Route that handles login logic
+    app.post('/produce', (req, res) => {
+        produceEvent(i.toString(), req.body.lat, req.body.lng)
+        i++
+        res.send("Message sent")
+    })
+
+
+    //Start your server on a specified port
+    app.listen(port, () => {
+        console.log(`Server is runing on port ${port}`)
+    })
+
+
+    function produceEvent(id, lat, lng) {
+        // call the `produce` function and log an error if it occurs
+        produce(id, lat, lng).catch((err) => {
+            console.error("error in producer: ", err)
+        })
+
+    }
 }
